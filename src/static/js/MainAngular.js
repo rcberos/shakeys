487b1c4d253d65101d2bf781f49aed0219e02456
rcberos <exia07@yahoo.com>
2018-01-31 14:56:16 +0800

    asdasd

--- src/static/js/MainAngular.js
+++ src/static/js/MainAngular.js
@@ -3,6 +3,10 @@ var app = angular.module('MainModule', ['ui.bootstrap']);
 app.controller('MainController', function($scope, $http, $timeout, $interval, $window){
 	
 
-	 $scope.Blaster = "YYYYY";
+	$scope.Blaster = "YYYYY";
+
+	$scope.fb_login = function(){
+		console.log('asdas');
+	}
 });
 
