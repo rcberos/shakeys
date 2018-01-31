4a0360aa3725e25fdd7539a3295c08591c0fcc02
rcberos <exia07@yahoo.com>
2018-01-31 15:07:30 +0800

    asdasd

--- src/static/js/MainAngular.js
+++ src/static/js/MainAngular.js
@@ -12,6 +12,7 @@ app.controller('MainController', function($scope, $http, $timeout, $interval, $w
 	        console.log(response);
 			if (response.status === 'connected') {
 				$scope.is_logged_in = 1;
+				console.log($scope.is_logged_in);
 				// console.log(response);
 
 	            fb_info.is_logged_in = 1;
